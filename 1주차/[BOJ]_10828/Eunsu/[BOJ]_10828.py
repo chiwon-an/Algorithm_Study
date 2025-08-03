0d81c50545f87bd5231d36ed5d1042a4cb7c11dd
김은수 <eunsue1222@gmail.com>
2025-08-03 22:21:51 +0900

    김은수 제출

---
+++ 1주차/[BOJ]_10828/Eunsu/[BOJ]_10828.py
@@ -0,0 +1,21 @@
+### 10828. 스택 ###
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+
+for _ in range(N):
+    cmd = input().split()
+
+    if cmd[0] == 'push':
+        stack.append(cmd[1])
+    elif cmd[0] == 'pop':
+        print(stack.pop() if stack else -1)
+    elif cmd[0] == 'size':
+        print(len(stack))
+    elif cmd[0] == 'empty':
+        print(0 if stack else 1)
+    elif cmd[0] == 'top':
+        print(stack[-1] if stack else -1)
